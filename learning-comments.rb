5149d097e40bc788ad1cc17bab7ff56aca8fc792
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2020-06-03 15:46:07 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,15 @@
+# run_code_inside = false 
+# puts "Code before if...end"
+# if run_code_inside 
+#  puts "code inside"
+# end 
+# puts "Code after if...end"
+
+chance_of_rain = 2.0 
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else 
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
